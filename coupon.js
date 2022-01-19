6b817f6e4a8a40f5dd09bb9ebec34d450c8ce262
tnyande <tnyande@gmail.com>
2022-01-19 08:40:54 +0000

    Coupon added

---
+++ coupon.js
@@ -0,0 +1,17 @@
+function closeDiv(){
+    document.getElementById('coupon').style.visibility="hidden"
+}
+
+
+function openDiv(){
+    document.getElementById('coupon').style.visibility="visible"
+}
+
+function changeColor(color){
+    if(color == 'red'){
+        document.getElementById('coupon').style.backgroundColor="red"
+    }else if(color == 'green')(
+        document.getElementById('coupon').style.backgroundColor="green"
+    )
+    
+}
